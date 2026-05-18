57849bf5b13d396fb2b8d20dac8d5f5c63e80bfb
alfianrht <alvianrht@gmail.com>
2026-05-19 03:35:35 +0700

    chore: use single app launcher icon

--- app/Views/partials/pwa_bootstrap.php
+++ app/Views/partials/pwa_bootstrap.php
@@ -4,4 +4,4 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="default">
     <meta name="apple-mobile-web-app-title" content="<?= esc($appName ?? 'Arus') ?>">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('images/logo-app1x.webp') ?>">
+    <link rel="apple-touch-icon" href="<?= base_url('images/logo-app1x.webp') ?>">
